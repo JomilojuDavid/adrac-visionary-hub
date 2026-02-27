be7847f0df7431eedb64498eda36a3f554a181bd
Jomiloju Wale-Adegboye <34211095+JomilojuDavid@users.noreply.github.com>
2026-02-27 12:22:04 +0100

    Add MotionDiv component for animated divs

---
+++ src/lib/animations/MotionDiv.tsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion";
+import { ReactNode } from "react";
+
+interface MotionDivProps {
+  children: ReactNode;
+  className?: string;
+  variant?: any;
+}
+
+const MotionDiv = ({
+  children,
+  className,
+  variant,
+}: MotionDivProps) => {
+  return (
+    <motion.div
+      variants={variant}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{
+        amount: 0.3,
+      }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MotionDiv;
